f12fd01b4c1961d4f08f250568283e2e6440bab6
Gunars Frickauss <gunarsf@gmail.com>
2026-05-06 13:52:08 +0300

    Update initkio.sh to expand rootfs without redirection
    Removed output redirection from raspi-config command.

--- initkio.sh
+++ initkio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 systemctl enable kiosk.service;
 systemctl disable firstboot.service;
-raspi-config --expand-rootfs > /dev/null;
+raspi-config --expand-rootfs;
 cp /etc/hosts /etc/hosts.backup;
 oldHostname=$(cat /proc/sys/kernel/hostname);
 ipo=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f2);
