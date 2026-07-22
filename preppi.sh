7484c21ca563be75423afffaed73df5ecc7ae44d
Gunars Frickauss <gunarsf@gmail.com>
2026-07-22 17:08:53 +0300

    Rename eth-speed.service to 10eth-speed.service

--- preppi.sh
+++ preppi.sh
@@ -84,5 +84,5 @@ touch "${WORK_DIR}kiosk.log"
 ln -s "${WORK_DIR}kiosk.log" "${USER_DIR}kiosk.log"
 ln "${WORK_DIR}firstboot.service" "/etc/systemd/system/firstboot.service"
 ln "${WORK_DIR}kiosk.service" "/etc/systemd/system/kiosk.service"
-ln "${WORK_DIR}eth-speed.service" "/etc/systemd/system/eth-speed.service"
+ln "${WORK_DIR}10eth-speed.service" "/etc/systemd/system/10eth-speed.service"
 systemctl enable firstboot.service
